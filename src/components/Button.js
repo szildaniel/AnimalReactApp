cfbeb4e9c394db3bbc20985e1feb8ff272d4cdb3
Daniel <szildaniel@gmail.com>
2020-01-10 14:52:43 +0100

    toDO in Tile component add onTransitionend event

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Button extends React.Component {
+    render() {
+        return ( 
+            <button type="button" className="btnLifeCycle">Check Lifecycle</button>
+        )
+    }
+}
+
+export default Button;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,28 +1,39 @@
 import React from "react";
+import Button from "./Button";
 
 class Tile extends React.Component {
     state = {
-        isActive: false
+        isActive: false,
+        showButton: false,
       };
+
+
   clickHandler = () => {
     const currentState = this.state.isActive;
     this.setState({ isActive: !currentState });
-
   };
+
+  toggleOpenActive = (event) => {
+    console.log(event);
+    const theState = this.state.showButton;
+    this.setState({ showButton: !theState})
+    console.log('transitionEnd')
+  }
   render() {
     return (
       <div
+        onTransitionEnd={this.toggleOpenActive}
         onClick={this.clickHandler}
-        // className={`tile ${this.props.uClass}`}>
+
          className={
           this.state.isActive
             ? `tile open ${this.props.uClass}`
             : `tile ${this.props.uClass}`}
     
       > 
-        <p>That</p>
-        <p>is</p>
+        <p>That is</p>
         <p>{this.props.name}</p>
+        <Button />
       </div>
     );
   }
