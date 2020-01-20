b832fef2390ab2b53bff5eef23b65c7d95942d7e
Daniel Mydlarz <szildaniel@gmail.com>
2020-01-20 18:34:28 +0100

    frog comp is styled in css toDO function that iterate life cycle and rest animals data

--- src/components/Frog.js
+++ src/components/Frog.js
@@ -1,16 +1,28 @@
 import React from "react";
 
 class Frog extends React.Component {
+  state = {
+    cycleNumber: 1,
+  }
+  nextCycle = () => {
+    console.log('there gonna be function')
+  }
   render() {
     const frog = this.props.location.state.frog;
     console.log(frog.name);
 
     return (
-      <div className="animal">
-        <h1>That's {frog.one.cycleName} of {frog.name}</h1>
-        <img src={frog.one.image} alt=""/>
-        <p>{frog.one.desc}</p>
-      </div>
+      <React.Fragment>
+        <div className="animal">
+          <h1>
+            That's {frog.name}s {frog.one.cycleName}
+          </h1>
+          <img src={frog.one.image} className="cycleImg" alt={frog.name} />
+          <p>{frog.one.desc}</p>
+          <button onClick={this.nextCycle} className="btnLifeCycle">Next</button>
+
+        </div>
+      </React.Fragment>
     );
   }
 }
