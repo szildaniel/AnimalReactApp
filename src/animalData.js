f59ee66667ab03d3263cb1e220dc1fee536d15b2
Daniel <szildaniel@gmail.com>
2020-01-21 14:42:05 +0100

    all data completed in animalData file

--- src/animalData.js
+++ src/animalData.js
@@ -33,8 +33,9 @@ const frog = {
   five: {
     cycleName: "Adult",
     image: "/images/cycles/frog/adult.jpg",
-    desc: "By between 12 to 16 weeks, depending on water and food supply, the frog has completed the full growth cycle."
-  },
+    desc:
+      "By between 12 to 16 weeks, depending on water and food supply, the frog has completed the full growth cycle."
+  }
 };
 
 const ladybug = {
@@ -42,22 +43,34 @@ const ladybug = {
   one: {
     cycleName: "Eggs",
     image: "/images/cycles/ladybug/eggs.jpg",
-    desc: "Lorem"
+    desc: `Probably you know what adult ladybugs look like, but you wouldn’t even recognize 
+    them before they get to that final stage of their lives. Female ladybugs lay their eggs on the underside of leaves. 
+    This is to protect them from being seen by flying predators as well as from the weather. A mother will lay
+    from 10-15 eggs in one place, they look similar to yellow jellybeans, except that they are tiny.`
   },
   two: {
     cycleName: "Larva",
     image: "/images/cycles/ladybug/larva.jpg",
-    desc: "loremx2"
+    desc: `Once the eggs hatch, the larvae will come out and start looking for something to eat. 
+    Newborn larvae look sort of like tiny alligators. After only a few days, the larvae will be large enough 
+    to begin to molt (shed their skin), and they keep molting for as long as they are growing. 
+    You can find the molted skins on leaves if you look very hard.`
   },
   three: {
     cycleName: "Pupa",
     image: "/images/cycles/ladybug/pupa.jpg",
-    desc: "loremx2"
+    desc: `After a couple of weeks of growing, the larvae will start to change into something that looks like a shrimp. 
+    It will find a leaf to attach itself to and it will seem to fall asleep for a few days, 
+    but it is not sleeping at all. 
+    During the pupa stage, the larvae are going through a metamorphosis into a ladybug.`
   },
   four: {
     cycleName: "Adult",
     image: "/images/cycles/ladybug/adult.jpg",
-    desc: "loremx2"
+    desc: `When the metamorphosis is complete, the skin of the larvae will split open and the full grown ladybug will emerge, 
+    but it still won’t look like the ladybug that you know so well. 
+    It will look soft and pink or very pale for a couple of hours until its shell becomes hard. 
+    As the shell hardens it also gains pigment, which causes the ladybug to become bright red.`
   }
 };
 
@@ -66,22 +79,28 @@ const bee = {
   one: {
     cycleName: "Eggs",
     image: "/images/cycles/bee/eggs.jpg",
-    desc: "Lorem"
+    desc: `The honey bee metamorphosis begins when the queen lays an egg. The eggs are mighty tiny only about 1.7 millimeters long. 
+    The queen lays a single egg in each cell that has been cleaned and prepared by the workers to raise new brood. 
+    The cell must be spotless, or she moves on to another one. Smaller cells are for female worker bees, and larger cells for male drone bees.`
   },
   two: {
     cycleName: "Larva",
     image: "/images/cycles/bee/larva.jpg",
-    desc: "loremx2"
+    desc: `Three days after the queen lays the egg, it hatches into a larva (the plural is larvae). 
+    Healthy larvae are snowy white and resemble small grubs curled up in the cells. Tiny at first, 
+    the larvae grow quickly, shedding their skin five times. These helpless little creatures have voracious appetites, consuming 1,300 meals a day.
+    Within just five days, they are 1,570 times larger than their original size.`
   },
   three: {
     cycleName: "Pupa",
     image: "/images/cycles/bee/pupa.jpg",
-    desc: "loremx2"
+    desc: `The larva is now officially a pupa (the plural is pupae). Here’s where things really begin to happen.The eyes, legs, and wings take shape. 
+    Coloration begins with the eyes: first pink, then purple, then black. Finally, the fine hairs that cover the bee’s body develop.`
   },
   four: {
     cycleName: "Adult",
     image: "/images/cycles/bee/adult.jpg",
-    desc: "loremx2"
+    desc: `After 12 days, the now adult bee chews her way through the wax capping to join her sisters and brothers.`
   }
 };
 
@@ -90,22 +109,31 @@ const bat = {
   one: {
     cycleName: "Birth",
     image: "/images/cycles/bat/birth.jpg",
-    desc: "Lorem"
+    desc: `A bat's pregnancy lasts between six and nine weeks. The length of the pregnancy depends on the species 	
+    and can be influenced by weather, climate and availability of food. Bats usually give birth to a single 
+    baby each year. For the first few weeks of their lives, baby bats cling to their mothers’ fur and drink milk as they grow.`
   },
   two: {
     cycleName: "Nursery",
     image: "/images/cycles/bat/nursery.jpg",
-    desc: "loremx2"
+    desc: `In big breeding colonies huge gatherings of baby-bats can be found; they are known as “nurseries” where females 
+    look after the young collectively. Clinging to the cave walls, the baby bats patiently wait for the return of their mothers.
+    When a female comes back to the roost, she finds her young faultlessly among the moving multitude. 
+    Once the two reunite, the baby attaches itself to her to feed.`
   },
   three: {
     cycleName: "Young Bat",
     image: "/images/cycles/bat/youngBat.jpg",
-    desc: "loremx2"
+    desc: `Most bats learn to fly and forage for food at three or four weeks old. Baby bats typically die from accidents while flying or falls from high nesting places. 
+    Many bats do not live to maturity. When winter approaches, some bats search for a place to hibernate 
+    while others migrate long distances to more seasonable climates.`
   },
   four: {
     cycleName: "Adult",
     image: "/images/cycles/bat/adult.jpg",
-    desc: "loremx2"
+    desc: `Bats are the most active at night between the hours of dusk to dawn. The adult bat live average 20 years.
+    The animals hunt during the night, primarily preying on small insects such as stone flies and mayflies. 
+    They capture their food with their teeth or by using their tail or wings to catch insects.`
   }
 };
 
@@ -114,22 +142,36 @@ const butterfly = {
   one: {
     cycleName: "Eggs",
     image: "/images/cycles/butterfly/eggs.jpg",
-    desc: "Lorem"
+    desc: `Eggs are laid on plants by the adult female butterfly. These plants will then become the food for the hatching caterpillars.
+    Eggs can be laid from spring, summer or fall. This depends on the species of butterfly. 
+    Females lay a lot of eggs at once so that at least some of them survive.
+    Butterfly eggs can be very small.`
   },
   two: {
-    cycleName: "Catepillar",
-    image: "/images/cycles/butterfly/catepillar.jpg",
-    desc: "loremx2"
+    cycleName: "Caterpillar",
+    image: "/images/cycles/butterfly/caterpillar.jpg",
+    desc: `The next stage is the larva. This is also called a caterpillar if the insect is a butterfly or a moth.
+    The job of the caterpillar is to eat and eat and eat. As the caterpillar grows it splits its skin and sheds it about 
+    4 or 5 times. Food eaten at this time is stored and used later as an adult. C
+    aterpillars can grow 100 times their size during this stage.`
   },
   three: {
     cycleName: "Chrysalis",
     image: "/images/cycles/butterfly/chrysalis.jpg",
-    desc: "loremx2"
+    desc: `When the caterpillar is full grown and stops eating, it becomes a pupa. The pupa of butterflies is also called a chrysalis.
+    This stage can last from a few weeks, a month or even longer. 
+    Some species have a pupal stage that lasts for two years. It may look like nothing is going on but big changes are happening inside. 
+    Special cells that were present in the larva are now growing rapidly. They will become the legs, wings, eyes 
+    and other parts of the adult butterfly. Many of the original larva cells will provide energy for these growing adult cells.`
   },
   four: {
     cycleName: "Adult",
     image: "/images/cycles/butterfly/adult.jpg",
-    desc: "loremx2"
+    desc: `The adult stage is what most people think of when they think of butterflies. They look very different from the larva.
+    The caterpillar has a few tiny eyes, stubby legs and very short antennae. 
+    The adults have long legs, long antennae, and compound eyes. 
+    They can also fly by using their large and colorful wings. The one thing they can't do is grow. 
+    The caterpillar's job was to eat. The adult's job is to mate and lay eggs. Most adult butterflies live only one or two weeks, but some species hibernate during the winter and may live several months.`
   }
 };
 
--- src/components/Frog.js
+++ src/components/Frog.js
@@ -8,17 +8,17 @@ class Frog extends React.Component {
     console.log('there gonna be function')
   }
   render() {
-    const frog = this.props.location.state.frog;
+    const frog = this.props.location.state.butterfly;
     console.log(frog.name);
 
     return (
       <React.Fragment>
         <div className="animal">
           <h1>
-            That's {frog.name}s {frog.one.cycleName}
+            {frog.four.cycleName}
           </h1>
-          <img src={frog.one.image} className="cycleImg" alt={frog.name} />
-          <p>{frog.one.desc}</p>
+          <img src={frog.four.image} className="cycleImg" alt={frog.name} />
+          <p>{frog.four.desc}</p>
           <button onClick={this.nextCycle} className="btnLifeCycle">Next</button>
 
         </div>
