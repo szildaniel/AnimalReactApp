61a78661207d67d96ccb183fb2ac9c689b7252f8
Daniel <szildaniel@gmail.com>
2020-01-30 14:20:25 +0100

    added CSSAnimation to slider, jumping buttons fixed with overflow:hidden, toDo fix last picture animation

--- src/components/AnimalSlider.js
+++ src/components/AnimalSlider.js
@@ -1,54 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
 
 const AnimalSlider = props => {
+  const name = props.name.toLowerCase();
+  const cycleLength = props.data[name].lifecycle.length;
 
   const [isMoreLifeCycle, setIsMoreLifeCycle] = useState(true);
   const [cycleNumber, setCycleNumber] = useState(0);
-
   const [inProp, setInProp] = useState(false);
 
-  const nextCycle = () => {
-
-    setInProp(!inProp);
-
-    const name = props.name.toLowerCase();
-    const lifeCycleArrLength = props.data[name].lifecycle.length;
-
-    if (cycleNumber + 1 < lifeCycleArrLength) {
-      setCycleNumber(cycleNumber + 1);
-      console.log(isMoreLifeCycle, lifeCycleArrLength, cycleNumber);
-    } else {
-      setCycleNumber(cycleNumber);
+  useEffect(() => {
+    if (cycleNumber === cycleLength - 1) {
       setIsMoreLifeCycle(!isMoreLifeCycle);
     }
+    return () => {
+      console.log("comp unmounted");
+    };
+  }, [cycleNumber]);
 
+
+  const nextCycle = () => {
+    
+
+    !inProp ? setInProp(!inProp) : setInProp(inProp);
+
+    if(cycleNumber < cycleLength - 1){
+      setCycleNumber(cycleNumber + 1);
+    }
+      else {
+        setCycleNumber(cycleNumber);
+        // setIsMoreLifeCycle(!isMoreLifeCycle)
+      }
     if (!isMoreLifeCycle) props.history.push("/App");
+    console.log(cycleNumber, cycleLength, isMoreLifeCycle);
+
   };
 
   const prevCycle = () => {
     cycleNumber > 0
       ? setCycleNumber(cycleNumber - 1)
       : setCycleNumber(cycleNumber);
-    const name = props.name.toLowerCase();
-    const lifeCycleArrLength = props.data[name].lifecycle.length;
 
-    if (cycleNumber < lifeCycleArrLength && !isMoreLifeCycle) {
+    if (cycleNumber < cycleLength && !isMoreLifeCycle) {
       setIsMoreLifeCycle(!isMoreLifeCycle);
     }
+    console.log(cycleNumber, cycleLength, isMoreLifeCycle);
+
   };
 
-  const name = props.name.toLowerCase();
   const index = cycleNumber;
-
   const animal = props.data[name];
-
   return (
     <React.Fragment>
       <TransitionGroup>
-        <CSSTransition in={inProp} timeout={1000} classNames="animal">
+        <CSSTransition
+          in={inProp}
+          key={uuid.v4()}
+          appear={false}
+          timeout={1000}
+          classNames="animal"
+        >
           <div className="animal">
             <h1 className="bangersFont"> {name} Life cycle!</h1>
             <figure>
@@ -56,6 +70,7 @@ const AnimalSlider = props => {
                 src={animal.lifecycle[index].image}
                 className="cycleImg"
                 alt={name}
+                key={uuid.v4()}
               />
 
               <figcaption>{animal.lifecycle[index].cycleName}</figcaption>
