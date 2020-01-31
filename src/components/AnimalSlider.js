6785ac0e3cbb25ea315052b63f70afabdb4986f3
Daniel <szildaniel@gmail.com>
2020-01-31 13:47:29 +0100

    useReducer hook is used for  logic in AnimalSlider now, button and animation works correctly

--- src/components/AnimalSlider.js
+++ src/components/AnimalSlider.js
@@ -1,64 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import { withRouter } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import uuid from "uuid";
+
+const initialState = {
+  i: 0,
+};
+
+function reducer(state, action) {
+  if (action.type === "increment") {
+    if (state.i < state.length-1) {
+      return { ...state, i: state.i + 1 };
+    } 
+    else { 
+      return {
+        ...state
+      };
+    }
+  }
+  if (action.type === "decrement") {
+    return { ...state, i: state.i - 1 };
+  }
+}
 
 const AnimalSlider = props => {
   const name = props.name.toLowerCase();
   const cycleLength = props.data[name].lifecycle.length;
 
-  const [isMoreLifeCycle, setIsMoreLifeCycle] = useState(true);
-  const [cycleNumber, setCycleNumber] = useState(0);
+  function newState() {
+    return {
+      i: 0,
+      name: name,
+      length: cycleLength,
+    };
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState, newState);
+
   const [inProp, setInProp] = useState(false);
 
-  useEffect(() => {
-    if (cycleNumber === cycleLength - 1) {
-      setIsMoreLifeCycle(!isMoreLifeCycle);
-    }
-    return () => {
-      console.log("comp unmounted");
-    };
-  }, [cycleNumber]);
+  const { image, cycleName, desc } = props.data[name].lifecycle[state.i];
 
-
-  const nextCycle = () => {
-    
-
-    !inProp ? setInProp(!inProp) : setInProp(inProp);
-
-    if(cycleNumber < cycleLength - 1){
-      setCycleNumber(cycleNumber + 1);
-    }
-      else {
-        setCycleNumber(cycleNumber);
-        // setIsMoreLifeCycle(!isMoreLifeCycle)
-      }
-    if (!isMoreLifeCycle) props.history.push("/App");
-    console.log(cycleNumber, cycleLength, isMoreLifeCycle);
-
-  };
-
-  const prevCycle = () => {
-    cycleNumber > 0
-      ? setCycleNumber(cycleNumber - 1)
-      : setCycleNumber(cycleNumber);
-
-    if (cycleNumber < cycleLength && !isMoreLifeCycle) {
-      setIsMoreLifeCycle(!isMoreLifeCycle);
-    }
-    console.log(cycleNumber, cycleLength, isMoreLifeCycle);
-
-  };
-
-  const index = cycleNumber;
-  const animal = props.data[name];
   return (
     <React.Fragment>
       <TransitionGroup>
         <CSSTransition
           in={inProp}
-          key={uuid.v4()}
+          key={state.i}
           appear={false}
           timeout={1000}
           classNames="animal"
@@ -66,36 +54,36 @@ const AnimalSlider = props => {
           <div className="animal">
             <h1 className="bangersFont"> {name} Life cycle!</h1>
             <figure>
-              <img
-                src={animal.lifecycle[index].image}
-                className="cycleImg"
-                alt={name}
-                key={uuid.v4()}
-              />
-
-              <figcaption>{animal.lifecycle[index].cycleName}</figcaption>
+              <img src={image} className="cycleImg" alt={name}/>
+              <figcaption>{cycleName}</figcaption>
             </figure>
 
-            <p>{animal.lifecycle[index].desc}</p>
+            <p>{desc}</p>
           </div>
         </CSSTransition>
       </TransitionGroup>
 
       <div className="btnContainer">
         <button
-          onClick={prevCycle}
+          onClick={() => dispatch({ type: "decrement" })}
           className="btnLifeCycle btnNav left"
-          disabled={cycleNumber === 0}
+          disabled={state.i === 0}
         >
           <FontAwesomeIcon icon="arrow-left" />
         </button>
-        <button onClick={nextCycle} className="btnLifeCycle right btnNav">
-          {isMoreLifeCycle ? (
+
+        {(state.i !== state.length-1) ?
+          <button onClick={() => dispatch({ type: "increment" })}
+                  className="btnLifeCycle right btnNav">
             <FontAwesomeIcon icon="arrow-right" />
-          ) : (
-            "Back to App"
-          )}
-        </button>
+          </button>
+          :
+          <button
+          onClick={() => props.history.push('/App')}
+          className="btnLifeCycle right btnNav">
+            Back to App
+          </button>
+         }
       </div>
     </React.Fragment>
   );
