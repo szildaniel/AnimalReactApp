444518941fa942c44496df8b0b3f0860017a55e1
Daniel Mydlarz <szildaniel@gmail.com>
2020-02-01 14:26:43 +0100

    Updates

---
+++ precache-manifest.311eed584d10b8f8a471482feb3f7ea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9f3abc4ec8c36d496e4f8bf30d6414",
+    "url": "/AnimalReactApp/index.html"
+  },
+  {
+    "revision": "b4947e6d0df416b0e336",
+    "url": "/AnimalReactApp/static/css/main.cc037882.chunk.css"
+  },
+  {
+    "revision": "7610b2b21343c7c72d0c",
+    "url": "/AnimalReactApp/static/js/2.7940704a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AnimalReactApp/static/js/2.7940704a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4947e6d0df416b0e336",
+    "url": "/AnimalReactApp/static/js/main.642be900.chunk.js"
+  },
+  {
+    "revision": "6308595c4c0f1eacd35f",
+    "url": "/AnimalReactApp/static/js/runtime-main.73ba9e13.js"
+  }
+]);
